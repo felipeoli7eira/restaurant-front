7cee6fba7f9bb6113a770e27d4e89b7e22a72cba
felipeoli7eira <me.felipeoliveira@gmail.com>
2024-03-01 11:58:18 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,23 @@ import { AppRoutes } from './routes'
 import { Theme } from './styles/Theme'
 import { GlobalStyle } from './styles/global'
 import { Normalize } from 'styled-normalize'
+import { PrimeReactProvider } from 'primereact/api'
+import { Button } from 'primereact/button'
+
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Theme>
-        <AppRoutes />
-        <GlobalStyle />
-        <Normalize />
-      </Theme>
-    </BrowserRouter>
+    <Button label="Check" icon="pi pi-check" />
+    // <BrowserRouter>
+    //   <PrimeReactProvider>
+    //     <Theme>
+    //       <AppRoutes />
+    //       <GlobalStyle />
+    //       <Normalize />
+    //     </Theme>
+    //   </PrimeReactProvider>
+    // </BrowserRouter>
   )
 }
