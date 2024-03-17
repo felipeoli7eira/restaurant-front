b4193b731cf4b1d41c836953250a97b997b041a3
felipeoli7eira <me.felipeoliveira@gmail.com>
2024-03-17 20:20:43 -0300

    util BRL format func

--- src/components/SnackStructure.tsx
+++ src/components/SnackStructure.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 import SnackPropsInterface from '../interfaces/snack/SnackPropsInterface'
 import { Component, SnakcCard } from './design'
 import { Button } from 'primereact/button'
+import { currencyBRL } from '../utils/formatBRL'
 
 const SnackStructure = ({ data }: SnackPropsInterface) => {
-  const reais = (value: number) =>
-    new Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' }).format(value)
-
   return (
     <Component className=''>
       {data.map((item, index) => (
@@ -22,7 +20,7 @@ const SnackStructure = ({ data }: SnackPropsInterface) => {
           />
 
           <div className='flex justify-content-between align-items-center px-3'>
-            <h3 className='text-secondary m-0 font-light text-right'>{reais(item.price)}</h3>
+            <h3 className='text-secondary m-0 font-light text-right'>{currencyBRL(item.price)}</h3>
             <Button
               rounded
               icon='pi pi-plus'
